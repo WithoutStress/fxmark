32c9b4b228fd6ce3ff484012c9b025e09728eb7e
CHOKYUJIN <bori19960@snu.ac.kr>
2023-05-02 07:25:30 +0000

    Fix syntax warning caused by incompatible python version (above v3.8)
    Signed-off-by: CHOKYUJIN <bori19960@snu.ac.kr>

--- bin/run-fxmark.py
+++ bin/run-fxmark.py
@@ -25,7 +25,7 @@ def catch_ctrl_C(sig, frame):
 class Runner(object):
     # media path
     LOOPDEV = "/dev/loopX"
-    NVMEDEV = "/dev/nvme0n1pX"
+    NVMEDEV = "/dev/nvmeXn1pX"
     HDDDEV  = "/dev/sdX"
     SSDDEV  = "/dev/sdY"
 
@@ -41,7 +41,7 @@ class Runner(object):
         self.CORE_GRAIN    = core_grain
         self.PERFMON_LEVEL = pfm_lvl
         self.FILTER        = run_filter # media, fs, bench, ncore, directio
-        self.DRYRUN        = False
+        self.DRYRUN        = True
         self.DEBUG_OUT     = False
 
         # bench config
@@ -234,7 +234,7 @@ class Runner(object):
         if self.active_ncore == ncore:
             return
         self.active_ncore = ncore
-        if ncore is 0:
+        if ncore == 0:
             ncores = "all"
         else:
             ncores = ','.join(map(lambda c: str(c), cpupol.seq_cores[0:ncore]))
@@ -272,7 +272,7 @@ class Runner(object):
     def umount(self, where):
         while True:
             p = self.exec_cmd("sudo umount " + where, self.dev_null)
-            if p.returncode is not 0:
+            if p.returncode != 0:
                 break
         (umount_hook, self.umount_hook) = (self.umount_hook, [])
         map(lambda hook: hook(), umount_hook);
@@ -329,16 +329,16 @@ class Runner(object):
                           + " " + self.HOWTO_MKFS.get(fs, "")
                           + " " + dev_path,
                           self.dev_null)
-        if p.returncode is not 0:
+        if p.returncode != 0:
             return False
         p = self.exec_cmd(' '.join(["sudo mount -t", fs,
                                     dev_path, mnt_path]),
                           self.dev_null)
-        if p.returncode is not 0:
+        if p.returncode != 0:
             return False
         p = self.exec_cmd("sudo chmod 777 " + mnt_path,
                           self.dev_null)
-        if p.returncode is not 0:
+        if p.returncode != 0:
             return False
         return True
 
@@ -351,20 +351,20 @@ class Runner(object):
                           + " " + self.HOWTO_MKFS.get(fs, "")
                           + " " + dev_path,
                           self.dev_null)
-        if p.returncode is not 0:
+        if p.returncode != 0:
             return False
         p = self.exec_cmd("sudo tune2fs -O ^has_journal %s" % dev_path,
                           self.dev_null)
-        if p.returncode is not 0:
+        if p.returncode != 0:
             return False
         p = self.exec_cmd(' '.join(["sudo mount -t ext4",
                                     dev_path, mnt_path]),
                           self.dev_null)
-        if p.returncode is not 0:
+        if p.returncode != 0:
             return False
         p = self.exec_cmd("sudo chmod 777 " + mnt_path,
                           self.dev_null)
-        if p.returncode is not 0:
+        if p.returncode != 0:
             return False
         return True
 
@@ -418,10 +418,10 @@ class Runner(object):
             os.path.join(self.log_dir,
                          '.'.join([media, fs, bench, str(nfg), "pm"])))
         (bin, type) = self.get_bin_type(bench)
-        directio = '1' if dio is "directio" else '0'
+        directio = '1' if dio == "directio" else '0'
 
-        if directio is '1':
-            if fs is "tmpfs": 
+        if directio == '1':
+            if fs == "tmpfs": 
                 print("# INFO: DirectIO under tmpfs disabled by default")
                 directio='0';
             else: 
@@ -515,14 +515,15 @@ if __name__ == "__main__":
 
     # TODO: make it scriptable
     run_config = [
-        (Runner.CORE_FINE_GRAIN,
-         PerfMon.LEVEL_LOW,
-         ("mem", "*", "DWOL", "80", "directio")),
-        # ("mem", "tmpfs", "filebench_varmail", "32", "directio")),
-        # (Runner.CORE_COARSE_GRAIN,
-        #  PerfMon.LEVEL_PERF_RECORD,
-        #  ("*", "*", "*", "*", "bufferedio")),
-        #
+        #(Runner.CORE_FINE_GRAIN,
+        # PerfMon.LEVEL_LOW,
+        # ("mem", "*", "DWOL", "80", "directio")),
+		# ("mem", "tmpfs", "filebench_varmail", "32", "directio")),
+
+        #(Runner.CORE_COARSE_GRAIN,
+        # PerfMon.LEVEL_PERF_RECORD,
+        # ("ssd", "*", "MWCL", "40", "bufferedio")),
+
         # (Runner.CORE_COARSE_GRAIN,
         #  PerfMon.LEVEL_PERF_RECORD,
         #  ("*", "*", "*", str(cpupol.PHYSICAL_CHIPS * cpupol.CORE_PER_CHIP), "*"))
